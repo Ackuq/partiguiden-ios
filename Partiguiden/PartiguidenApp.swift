184567eef60518b2e697ef988cdd66125a31eb1d
Axel Pettersson <axel@pettersson.cc>
2022-03-03 19:24:00 +0100

    Initial Commit

---
+++ Partiguiden/PartiguidenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartiguidenApp.swift
+//  Partiguiden
+//
+//  Created by Axel Pettersson on 2022-03-03.
+//
+
+import SwiftUI
+
+@main
+struct PartiguidenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
